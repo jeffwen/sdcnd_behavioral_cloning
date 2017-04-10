ca2895c2fd6456ca5d88dd372b41803daa28ffa7
Jeff Wen <jeff.li.wen@gmail.com>
2017-04-10 20:26:30 +0000

    changed files and update generation function

---
+++ data.py
@@ -0,0 +1,81 @@
+import cv2
+import pandas as pd
+import numpy as np
+import csv
+import platform
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+ON_SERVER = False
+if platform.system() == "Linux":
+    col_names = ["center","left","right","steering","throttle","brake","speed"]
+    proj_path = '/home/carnd/sdcnd_behavioral_cloning/'
+    folder_path = '/home/carnd/sdcnd_behavioral_cloning/training_video_log/driving_log.csv'
+    ON_SERVER = True
+elif platform.system() == "Darwin" and platform.uname()[1] == 'C02RH2F7G8WM':
+    folder_path = '/Users/jwen/Python/Projects/sdcnd_behavioral_cloning/training_video_log/driving_log.csv'
+    proj_path = '/Users/jwen/Python/Projects/sdcnd_behavioral_cloning/'
+elif platform.system() == "Darwin":
+    folder_path = '/Users/Jeffwen/Documents/Online Courses/sdcnd_behavioral_cloning/training_video_log/driving_log.csv'
+    proj_path = '/Users/Jeffwen/Documents/Online Courses/sdcnd_behavioral_cloning/'
+else:
+    print("Unknown environment")
+
+def read_input(folder_path):
+
+    lines = []
+     
+    ## read in the csv file with images and steering data
+    with open(folder_path) as csv_file:
+        reader = csv.reader(csv_file)
+        for line in reader:
+            lines.append(line)
+
+    ## shuffle the observations 
+    lines_shuffled = shuffle(lines)
+
+    ## split into training and validation data sets
+    train_lines, validation_lines = train_test_split(lines_shuffled, test_size = 0.2)
+            
+    return train_lines, validation_lines
+
+def generate_data(observations, batch_size = 48):
+
+    ## set up generator
+    while True:
+        for offset in range(0, len(observations), batch_size):
+            batch_obs = observations[offset:offset+batch_size]
+
+            center_images = []
+            left_images = []
+            right_images = []
+
+            steering_angle_center = []
+            steering_angle_left = []
+            steering_angle_right = []
+
+            steering_correction = 0.25 ## applying correction to left and right steering angles
+
+            ## loop through lines and append images path/ steering data to new lists
+            for observation in tqdm(batch_obs):
+                center_image_path = proj_path + 'training_video_log/IMG/'+observation[0].split('/')[-1]
+                left_image_path = proj_path + 'training_video_log/IMG/'+observation[1].split('/')[-1]
+                right_image_path = proj_path + 'training_video_log/IMG/'+observation[2].split('/')[-1]
+                
+                center_images.append(cv2.imread(center_image_path))
+                left_images.append(cv2.imread(left_image_path))
+                right_images.append(cv2.imread(right_image_path))
+        
+                ## append the steering angles and correct for left/right images
+                steering_angle_center.append(float(line[3]))
+                steering_angle_left.append(float(line[3]) + steering_correction)
+                steering_angle_right.append(float(line[3]) - steering_correction)
+        
+            images = center_images + left_images + right_images
+            steering_angles = steering_angle_center + steering_angle_left + steering_angle_right
+
+            X = np.array(images)
+            y = np.array(steering_angles)
+
+            yield shuffle(X, y)
