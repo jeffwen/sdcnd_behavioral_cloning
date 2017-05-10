f535e4511b1b957d67c92cb3625c290046362e5b
Jeff Wen <jeff.li.wen@gmail.com>
2017-05-10 12:23:13 -0700

    updated to use existing model for training

--- data.py
+++ data.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
 if platform.system() == "Linux":
-    col_names = ["center","left","right","steering","throttle","brake","speed"]
+    col_names = ["center", "left", "right", "steering", "throttle", "brake", "speed"]
     proj_path = '/home/carnd/sdcnd_behavioral_cloning/'
     folder_path = '/home/carnd/sdcnd_behavioral_cloning/training_video_log/driving_log.csv'
 elif platform.system() == "Darwin" and platform.uname()[1] == 'C02RH2F7G8WM':
@@ -23,23 +23,23 @@ else:
 def read_input(folder_path):
 
     lines = []
-     
+
     ## read in the csv file with images and steering data
     with open(folder_path) as csv_file:
         reader = csv.reader(csv_file)
         for line in reader:
             lines.append(line)
 
-    ## shuffle the observations 
+    ## shuffle the observations
     lines_shuffled = shuffle(lines)
 
     ## split into training and validation data sets
     train_lines, validation_lines = train_test_split(lines_shuffled, test_size = 0.2)
-            
+
     return train_lines, validation_lines
 
 
-def generate_data(observations, batch_size = 48):
+def generate_data(observations, batch_size=48):
 
     ## set up generator
     while True:
@@ -61,16 +61,16 @@ def generate_data(observations, batch_size = 48):
                 center_image_path = proj_path + 'training_video_log/IMG/'+observation[0].split('/')[-1]
                 left_image_path = proj_path + 'training_video_log/IMG/'+observation[1].split('/')[-1]
                 right_image_path = proj_path + 'training_video_log/IMG/'+observation[2].split('/')[-1]
-                
+
                 center_images.append(cv2.imread(center_image_path))
                 left_images.append(cv2.imread(left_image_path))
                 right_images.append(cv2.imread(right_image_path))
-        
+
                 ## append the steering angles and correct for left/right images
                 steering_angle_center.append(float(observation[3]))
                 steering_angle_left.append(float(observation[3]) + steering_correction)
                 steering_angle_right.append(float(observation[3]) - steering_correction)
-        
+
             images = center_images + left_images + right_images
             steering_angles = steering_angle_center + steering_angle_left + steering_angle_right
 
--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 import numpy as np
 import data
 from matplotlib import pyplot as plt
+import seaborn as sns
 import platform
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Conv2D, MaxPooling2D, Cropping2D
 from keras.layers import Activation, Dropout, Flatten, Dense, BatchNormalization
 from keras.callbacks import ModelCheckpoint
@@ -30,39 +31,42 @@ train_generator = data.generate_data(train_observations)
 validation_generator = data.generate_data(validation_observations)
 
 
-## build Keras model 
-model = Sequential()
+# ## build Keras model
+# model = Sequential()
+#
+# ## cropping images
+# model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+#
+# ## batch normlization
+# model.add(BatchNormalization())
+#
+# ## Convolutional layers
+# model.add(Conv2D(24, 5, 5, subsample=(2,2), activation='relu'))
+# model.add(Conv2D(36, 5, 5, subsample=(2,2), activation='relu'))
+# model.add(Conv2D(48, 5, 5, subsample=(2,2), activation='relu'))
+# model.add(Conv2D(64, 3, 3, subsample=(1,1), activation='relu'))
+# model.add(Conv2D(64, 3, 3, subsample=(1,1), activation='relu'))
+#
+# ## fully connected layers
+# model.add(Flatten())
+# model.add(Dense(1164, activation='relu'))
+# model.add(Dense(100, activation='relu'))
+# model.add(Dense(50, activation='relu'))
+# model.add(Dense(10, activation='relu'))
+# model.add(Dense(1))
+#
+# ## compile
+# model.compile(loss='mse', optimizer='adam')
 
-## cropping images
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
-
-## batch normlization
-model.add(BatchNormalization())
-
-## Convolutional layers
-model.add(Conv2D(24, 5, 5, subsample=(2,2), activation='relu'))
-model.add(Conv2D(36, 5, 5, subsample=(2,2), activation='relu'))
-model.add(Conv2D(48, 5, 5, subsample=(2,2), activation='relu'))
-model.add(Conv2D(64, 3, 3, subsample=(1,1), activation='relu'))
-model.add(Conv2D(64, 3, 3, subsample=(1,1), activation='relu'))
-
-## fully connected layers
-model.add(Flatten())
-model.add(Dense(1164, activation='relu'))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(50, activation='relu'))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(1))
-
-## compile
-model.compile(loss='mse', optimizer='adam')
+## load and continue training the pre-trained model on more data
+model = load_model(proj_path + "model.h5")
 
 # checkpoint and save best model
-model_path="model.h5"
+model_path="model_new.h5"
 checkpoint = ModelCheckpoint(model_path, verbose=0, save_best_only=True)
 callbacks_list = [checkpoint]
 
 ## fit the model
 model.fit_generator(train_generator, samples_per_epoch=len(train_observations), validation_data=validation_generator,
-                    nb_val_samples=len(validation_observations), nb_epoch=5, callbacks = callbacks_list)
+                    nb_val_samples=len(validation_observations), nb_epoch=5, callbacks=callbacks_list)
 
